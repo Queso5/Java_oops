0f01abfea0067c9c874cf3e74eeaf6a84071507c
[tejas parate] <tejasparate.st@gmail.com>
2025-08-06 18:38:42 +0530

    day 1

---
+++ src/Student.java
@@ -0,0 +1,32 @@
+public class Student {
+    //Class is the blueprint (collection of objects is called a class)
+    //objects are real world real world instances of the class
+    String name ;
+    int age ;
+    int rollNo ;
+
+    //Constructors
+    Student(String name , int age , int rollNo){
+        this.name = name;
+        this.age = age;
+        this.rollNo = rollNo;
+
+    }
+
+    //method to display
+    void displayDetails(){
+        System.out.println("name:" + name);
+        System.out.println("age:"+ age);
+        System.out.println("rollNo:" + rollNo);
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("tejas", 21, 64);
+        s1.displayDetails();
+
+        Student s2 = new Student("Naman" , 20 , 53);
+        s2.displayDetails();
+    }
+}
+
+
